ff525ce7a240e5e7a66424d9dd95da902f7463f4
KotlyarovSergey <zews-sv@rambler.ru>
2018-12-21 13:12:58 +0300

    trouble 1

--- InternetControll/Form1.Designer.cs
+++ InternetControll/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmBoxInterFaces = new System.Windows.Forms.ComboBox();
             this.lblDNS = new System.Windows.Forms.Label();
             this.lblGatWay = new System.Windows.Forms.Label();
             this.lblMask = new System.Windows.Forms.Label();
@@ -58,6 +59,7 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.cmBoxInterFaces);
             this.groupBox1.Controls.Add(this.lblDNS);
             this.groupBox1.Controls.Add(this.lblGatWay);
             this.groupBox1.Controls.Add(this.lblMask);
@@ -69,6 +71,16 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Status";
+            // 
+            // cmBoxInterFaces
+            // 
+            this.cmBoxInterFaces.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmBoxInterFaces.FormattingEnabled = true;
+            this.cmBoxInterFaces.Location = new System.Drawing.Point(70, 19);
+            this.cmBoxInterFaces.Name = "cmBoxInterFaces";
+            this.cmBoxInterFaces.Size = new System.Drawing.Size(182, 21);
+            this.cmBoxInterFaces.TabIndex = 5;
+            this.cmBoxInterFaces.SelectedIndexChanged += new System.EventHandler(this.cmBoxInterFaces_SelectedIndexChanged);
             // 
             // lblDNS
             // 
@@ -86,7 +98,7 @@
             this.lblGatWay.Name = "lblGatWay";
             this.lblGatWay.Size = new System.Drawing.Size(55, 13);
             this.lblGatWay.TabIndex = 3;
-            this.lblGatWay.Text = "GeteWay:";
+            this.lblGatWay.Text = "GateWay:";
             // 
             // lblMask
             // 
@@ -109,7 +121,7 @@
             // lblInterface
             // 
             this.lblInterface.AutoSize = true;
-            this.lblInterface.Location = new System.Drawing.Point(23, 35);
+            this.lblInterface.Location = new System.Drawing.Point(12, 24);
             this.lblInterface.Name = "lblInterface";
             this.lblInterface.Size = new System.Drawing.Size(52, 13);
             this.lblInterface.TabIndex = 0;
@@ -117,7 +129,6 @@
             // 
             // btnGetStatus
             // 
-            this.btnGetStatus.Enabled = false;
             this.btnGetStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.btnGetStatus.Location = new System.Drawing.Point(321, 36);
             this.btnGetStatus.Name = "btnGetStatus";
@@ -125,6 +136,7 @@
             this.btnGetStatus.TabIndex = 1;
             this.btnGetStatus.Text = "<---  Get status";
             this.btnGetStatus.UseVisualStyleBackColor = true;
+            this.btnGetStatus.Click += new System.EventHandler(this.btnGetStatus_Click);
             // 
             // groupBox2
             // 
@@ -143,7 +155,6 @@
             // checkBoxPingGateWay
             // 
             this.checkBoxPingGateWay.AutoSize = true;
-            this.checkBoxPingGateWay.Enabled = false;
             this.checkBoxPingGateWay.Location = new System.Drawing.Point(23, 87);
             this.checkBoxPingGateWay.Name = "checkBoxPingGateWay";
             this.checkBoxPingGateWay.Size = new System.Drawing.Size(94, 17);
@@ -271,6 +282,7 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "InternetControll";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.Resize += new System.EventHandler(this.Form1_Resize);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
@@ -306,6 +318,7 @@
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ComboBox cmBoxInterFaces;
     }
 }
 
--- InternetControll/Form1.cs
+++ InternetControll/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
 using System.Net.NetworkInformation;
 
 namespace InternetControll
@@ -19,6 +20,13 @@ namespace InternetControll
         }
         private int tryPingCount = 0;
         private bool isRunning = false;
+        private IPAddress defGateWay;
+        private enum pingStatus
+        {
+            siteOK,
+            gateWayOK,
+            lost
+        };
 
         private void btnStart_Click(object sender, EventArgs e)
         {
@@ -40,7 +48,7 @@ namespace InternetControll
                 lblPingLabel.Visible = true;
                 lblPing.Visible = true;
                 // pinger run
-                pinger(true);
+                pinger();
                 timerOfPing.Start();
                 notifyIcon1.Visible = true;
             }
@@ -50,27 +58,87 @@ namespace InternetControll
             isRunning = !isRunning;
         }
 
-        private void pinger(bool run)
+        private void pinger()
         {
             Ping png = new Ping();
             PingReply pngReply;
             tryPingCount++;
             toolStripStatusLabel1.Text = "attempts to ping: " + tryPingCount.ToString();
+            // try ping site
             try
             {
-                pngReply = png.Send(txbPingSite.Text.Trim());
+                pngReply = png.Send(txbPingSite.Text.Trim(),1500);
             }
             catch
             {
-                changePingStatusOK(false);
+                changePingStatusOK(pingStatus.lost);
                 return;
             }
+            // if ping site success
             if (pngReply.Status == IPStatus.Success)
             {
-                changePingStatusOK(true);
+                changePingStatusOK(pingStatus.siteOK);
             }
-            else
-                changePingStatusOK(false);
+            else // if ping site lost
+            {
+                // if cheked ping gateway
+                if (checkBoxPingGateWay.Checked == true)
+                {
+                    // try ping GateWay
+                    try
+                    {
+                        pngReply = png.Send(defGateWay);
+                    }
+                    catch
+                    {
+                        changePingStatusOK(pingStatus.lost);
+                        return;
+                    }
+
+                    // if ping GateWay success
+                    if (pngReply.Status == IPStatus.Success)
+                    {
+                        changePingStatusOK(pingStatus.gateWayOK);
+                    }
+                }
+                else  // if don't ping gateway
+                {
+                    changePingStatusOK(pingStatus.lost);
+                }
+            }
+        }
+
+        private void changePingStatusOK(pingStatus pStat)
+        {
+            switch(pStat)
+            {
+                case pingStatus.siteOK:
+                    if (lblPing.Text.CompareTo("OK") != 0) // iw now not OK
+                    {
+                        lblPing.Text = "OK";
+                        lblPing.ForeColor = System.Drawing.Color.Green;
+                        notifyIcon1.Icon = Properties.Resources.pingok;
+                    }
+                    break;
+
+                case pingStatus.gateWayOK:
+                    if (lblPing.Text.CompareTo("only Gate") != 0) // iw now not OK
+                    {
+                        lblPing.Text = "only Gate";
+                        lblPing.ForeColor = System.Drawing.Color.Yellow;
+                        notifyIcon1.Icon = Properties.Resources.pingok;
+                    }
+                    break;
+
+                default:
+                    if (lblPing.Text.CompareTo("Lost") != 0) // iw now not OK
+                    {
+                        lblPing.Text = "Lost";
+                        lblPing.ForeColor = System.Drawing.Color.Red;
+                        notifyIcon1.Icon = Properties.Resources.pinglost;
+                    }
+                    break;
+            }
         }
 
         private void changePingStatusOK(bool isOK)
@@ -95,9 +163,11 @@ namespace InternetControll
             }
         }
 
+
         private void timerOfPing_Tick(object sender, EventArgs e)
         {
-            pinger(true);
+            timerOfPing.Stop();
+            pinger();
         }
 
         private void Form1_Resize(object sender, EventArgs e)
@@ -115,6 +185,95 @@ namespace InternetControll
                 this.Show();
                 WindowState = FormWindowState.Normal;
             }
+        }
+
+
+        // get IP Config
+        private void btnGetStatus_Click(object sender, EventArgs e)
+        {
+            getIFStatus();
+        }
+
+        private void getIFStatus()
+        {
+            // get all IF and fill combo
+            cmBoxInterFaces.Items.Clear();
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if ((nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet) ||
+                    (nic.NetworkInterfaceType == NetworkInterfaceType.Wireless80211))
+                {
+                    cmBoxInterFaces.Items.Add(nic.Description);
+                }
+            }
+            if (cmBoxInterFaces.Items.Count > 0)
+            {
+                cmBoxInterFaces.SelectedIndex = 0;
+            }
+            else
+            {
+                return;
+            }
+
+        }
+
+
+        // selected InterFace changed
+        private void cmBoxInterFaces_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //lblIP.Text = "IP: "; 
+
+            foreach (NetworkInterface NI in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                foreach (UnicastIPAddressInformation ip in NI.GetIPProperties().UnicastAddresses)
+                {
+                    if (NI.Description == cmBoxInterFaces.SelectedItem.ToString())
+                    {
+                        if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                        {
+                            //MessageBox.Show(ip.Address.ToString());
+                            lblIP.Text = "IP: " + ip.Address.ToString();
+                            lblMask.Text = "Mask: " + ip.IPv4Mask.ToString();
+                        }
+                        IPInterfaceProperties ipifpr = NI.GetIPProperties();
+                        lblDNS.Text = getDNS(ipifpr);
+                        lblGatWay.Text = getGateWay(ipifpr);
+                    }
+                }
+            }
+        }
+
+        private string getDNS(IPInterfaceProperties IPIFProp)
+        {
+            IPAddressCollection IPColl = IPIFProp.DnsAddresses;
+            foreach (IPAddress ip in IPColl)
+            {
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    return "DNS: " + ip.ToString();
+                }
+            }
+
+            return "DNS: ";
+        }
+
+        private string getGateWay(IPInterfaceProperties IPIFProp)
+        {
+            GatewayIPAddressInformationCollection GIPAIC = IPIFProp.GatewayAddresses;
+            foreach  (GatewayIPAddressInformation iP in GIPAIC)
+            {
+                if (iP.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    defGateWay = iP.Address;
+                    return "GateWay: " + defGateWay.ToString();
+                }
+            }
+            return "GateWay:";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getIFStatus();
         }
     }
 }
